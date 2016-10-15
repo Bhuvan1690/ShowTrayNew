71d6dfd4621a82dbbb97fc8fbc76450569fc8319
Ashwani <Ashwani@Ashwanis-MacBook-Pro.local>
2016-10-15 13:05:42 -0700

    Initial Commit

---
+++ ShowTray/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShowTray
+//
+//  Created by Ashwani on 15/10/16.
+//  Copyright © 2016 Ashwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
